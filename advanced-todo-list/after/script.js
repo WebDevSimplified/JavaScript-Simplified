101d92789e8ff442d57c39823f1d0d64b7920b62
Web Dev Simplified <webdevsimplified@gmail.com>
2020-10-21 16:02:39 -0500

    Finish Advanced Todo List

--- advanced-todo-list/after/script.js
+++ advanced-todo-list/after/script.js
@@ -1,49 +1,50 @@
-const list = document.querySelector("#list")
-const newTodoForm = document.querySelector("#new-todo-form")
+const form = document.querySelector("#new-todo-form")
 const todoInput = document.querySelector("#todo-input")
-const listItemTemplate = document.querySelector("#list-item-template")
-const STORAGE_PREFIX = "ADVANCED_TODO_LIST"
-const TODO_KEY = `${STORAGE_PREFIX}-todos`
-
+const list = document.querySelector("#list")
+const template = document.querySelector("#list-item-template")
+const LOCAL_STORAGE_PREFIX = "ADVANCED_TODO_LIST"
+const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`
 let todos = loadTodos()
 todos.forEach(renderTodo)
-
-list.addEventListener("click", e => {
-  if (!e.target.matches("[data-button-delete]")) return
-
-  const parent = e.target.closest(".list-item")
-  todos = todos.filter(todo => todo.id !== parent.dataset.todoId)
-  parent.remove()
-  saveTodos()
-})
 
 list.addEventListener("change", e => {
   if (!e.target.matches("[data-list-item-checkbox]")) return
 
   const parent = e.target.closest(".list-item")
-  const todo = todos.find(t => t.id === parent.dataset.todoId)
+  const todoId = parent.dataset.todoId
+  const todo = todos.find(t => t.id === todoId)
   todo.complete = e.target.checked
   saveTodos()
 })
 
-newTodoForm.addEventListener("submit", e => {
+list.addEventListener("click", e => {
+  if (!e.target.matches("[data-button-delete]")) return
+
+  const parent = e.target.closest(".list-item")
+  const todoId = parent.dataset.todoId
+  parent.remove()
+  todos = todos.filter(todo => todo.id !== todoId)
+  saveTodos()
+})
+
+form.addEventListener("submit", e => {
   e.preventDefault()
 
-  if (todoInput.value !== "") {
-    const newTodo = {
-      name: todoInput.value,
-      checked: false,
-      id: new Date().valueOf().toString()
-    }
-    todos.push(newTodo)
-    renderTodo(newTodo)
-    todoInput.value = ""
-    saveTodos()
+  const todoName = todoInput.value
+  if (todoName === "") return
+  const newTodo = {
+    name: todoName,
+    complete: false,
+    id: new Date().valueOf().toString()
   }
+  todos.push(newTodo)
+  renderTodo(newTodo)
+  saveTodos()
+  todoInput.value = ""
 })
 
 function renderTodo(todo) {
-  const templateClone = listItemTemplate.content.cloneNode(true)
+  const templateClone = template.content.cloneNode(true)
   const listItem = templateClone.querySelector(".list-item")
   listItem.dataset.todoId = todo.id
   const textElement = templateClone.querySelector("[data-list-item-text]")
@@ -53,11 +54,11 @@ function renderTodo(todo) {
   list.appendChild(templateClone)
 }
 
-function saveTodos() {
-  localStorage.setItem(TODO_KEY, JSON.stringify(todos))
+function loadTodos() {
+  const todosString = localStorage.getItem(TODOS_STORAGE_KEY)
+  return JSON.parse(todosString) || []
 }
 
-function loadTodos() {
-  const todosString = localStorage.getItem(TODO_KEY)
-  return JSON.parse(todosString) || []
+function saveTodos() {
+  localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos))
 }
