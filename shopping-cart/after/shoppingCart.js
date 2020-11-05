1b4a7c297bfa6be350ee47f3b3effcafe2360756
Web Dev Simplified <webdevsimplified@gmail.com>
2020-11-05 16:01:27 -0600

    Finalize Shopping Cart Video:

--- shopping-cart/after/shoppingCart.js
+++ shopping-cart/after/shoppingCart.js
@@ -1,26 +1,58 @@
-import addGlobalEventListener from "./utils/addGlobalEventListener"
-import formatCurrency from "./utils/formatCurrency"
 import items from "./items.json"
+import formatCurrency from "./util/formatCurrency.js"
+import addGlobalEventListener from "./util/addGlobalEventListener.js"
 
-const STORAGE_KEY = "SHOPPING_CART-cart"
-const IMAGE_URL = "https://dummyimage.com/210x130"
-let shoppingCart = loadCart()
-const cartElement = document.querySelector("[data-cart]")
-const cartItemsElement = document.querySelector("[data-cart-items]")
 const cartButton = document.querySelector("[data-cart-button]")
+const cartItemsWrapper = document.querySelector("[data-cart-items-wrapper]")
+let shoppingCart = []
+const IMAGE_URL = "https://dummyimage.com/210x130"
 const cartItemTemplate = document.querySelector("#cart-item-template")
-const cartTotal = document.querySelector("[data-cart-total]")
+const cartItemContainer = document.querySelector("[data-cart-items]")
 const cartQuantity = document.querySelector("[data-cart-quantity]")
-const cartItemsWrapper = document.querySelector("[data-cart-items-wrapper")
+const cartTotal = document.querySelector("[data-cart-total]")
+const cart = document.querySelector("[data-cart]")
+const SESSION_STORAGE_KEY = "SHOPPING_CART-cart"
 
 export function setupShoppingCart() {
   addGlobalEventListener("click", "[data-remove-from-cart-button]", e => {
-    const itemId = parseInt(e.target.closest("[data-item]").dataset.itemId)
-    removeFromCart(itemId)
+    const id = parseInt(e.target.closest("[data-item]").dataset.itemId)
+    removeFromCart(id)
   })
 
+  shoppingCart = loadCart()
   renderCart()
-  cartButton.addEventListener("click", toggleCartOpen)
+
+  cartButton.addEventListener("click", () => [
+    cartItemsWrapper.classList.toggle("invisible")
+  ])
+}
+
+function saveCart() {
+  sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(shoppingCart))
+}
+
+function loadCart() {
+  const cart = sessionStorage.getItem(SESSION_STORAGE_KEY)
+  return JSON.parse(cart) || []
+}
+
+export function addToCart(id) {
+  const existingItem = shoppingCart.find(entry => entry.id === id)
+  if (existingItem) {
+    existingItem.quantity++
+  } else {
+    shoppingCart.push({ id: id, quantity: 1 })
+  }
+  renderCart()
+  saveCart()
+}
+
+function removeFromCart(id) {
+  const existingItem = shoppingCart.find(entry => entry.id === id)
+  if (existingItem == null) return
+  shoppingCart = shoppingCart.filter(entry => entry.id !== id)
+  renderCart()
+  saveCart()
 }
 
 function renderCart() {
@@ -32,41 +64,38 @@ function renderCart() {
   }
 }
 
-export function addToCart(id) {
-  const existingEntry = shoppingCart.find(entry => entry.id === id)
-  if (existingEntry) {
-    existingEntry.quantity++
-  } else {
-    shoppingCart.push({ id: id, quantity: 1 })
-  }
-  saveCart()
-  renderCart()
+function hideCart() {
+  cart.classList.add("invisible")
+  cartItemsWrapper.classList.add("invisible")
 }
 
-function removeFromCart(id) {
-  const existingEntry = shoppingCart.find(entry => entry.id === id)
-  console.log(existingEntry)
-  if (existingEntry == null) return
-  shoppingCart = shoppingCart.filter(entry => entry.id !== id)
-  saveCart()
-  renderCart()
+function showCart() {
+  cart.classList.remove("invisible")
 }
 
 function renderCartItems() {
-  cartItemsElement.innerHTML = ""
+  cartQuantity.innerText = shoppingCart.length
 
+  const totalCents = shoppingCart.reduce((sum, entry) => {
+    const item = items.find(i => entry.id === i.id)
+    return sum + item.priceCents * entry.quantity
+  }, 0)
+
+  cartTotal.innerText = formatCurrency(totalCents / 100)
+
+  cartItemContainer.innerHTML = ""
   shoppingCart.forEach(entry => {
+    const item = items.find(i => entry.id === i.id)
     const cartItem = cartItemTemplate.content.cloneNode(true)
-    const item = items.find(i => i.id === entry.id)
 
     const container = cartItem.querySelector("[data-item]")
-    container.dataset.itemId = entry.id
-
-    const image = cartItem.querySelector("[data-image]")
-    image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
+    container.dataset.itemId = item.id
 
     const name = cartItem.querySelector("[data-name]")
     name.innerText = item.name
+
+    const image = cartItem.querySelector("[data-image]")
+    image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
 
     if (entry.quantity > 1) {
       const quantity = cartItem.querySelector("[data-quantity]")
@@ -76,36 +105,6 @@ function renderCartItems() {
     const price = cartItem.querySelector("[data-price]")
     price.innerText = formatCurrency((item.priceCents * entry.quantity) / 100)
 
-    cartItemsElement.appendChild(cartItem)
+    cartItemContainer.appendChild(cartItem)
   })
-
-  const totalCents = shoppingCart.reduce((sum, entry) => {
-    const item = items.find(i => i.id === entry.id)
-    return sum + item.priceCents * entry.quantity
-  }, 0)
-  cartTotal.innerText = formatCurrency(totalCents / 100)
-
-  cartQuantity.innerHTML = shoppingCart.length
-}
-
-function toggleCartOpen() {
-  cartItemsWrapper.classList.toggle("invisible")
-}
-
-function hideCart() {
-  cartElement.classList.add("invisible")
-  cartItemsWrapper.classList.add("invisible")
-}
-
-function showCart() {
-  cartElement.classList.remove("invisible")
-}
-
-function saveCart() {
-  sessionStorage.setItem(STORAGE_KEY, JSON.stringify(shoppingCart))
-}
-
-function loadCart() {
-  const cart = sessionStorage.getItem(STORAGE_KEY)
-  return JSON.parse(cart) || []
 }
--- shopping-cart/after/store.js
+++ shopping-cart/after/store.js
@@ -1,18 +1,18 @@
-import formatCurrency from "./utils/formatCurrency"
-import addGlobalEventListener from "./utils/addGlobalEventListener"
 import items from "./items.json"
-import { addToCart } from "./shoppingCart"
+import formatCurrency from "./util/formatCurrency.js"
+import addGlobalEventListener from "./util/addGlobalEventListener.js"
+import { addToCart } from "./shoppingCart.js"
 
 const storeItemTemplate = document.querySelector("#store-item-template")
-const storeItems = document.querySelector("[data-store-items]")
+const storeItemContainer = document.querySelector("[data-store-container]")
 const IMAGE_URL = "https://dummyimage.com/420x260"
 
 export function setupStore() {
-  if (storeItems == null) return
+  if (storeItemContainer == null) return
 
   addGlobalEventListener("click", "[data-add-to-cart-button]", e => {
-    const itemId = parseInt(e.target.closest("[data-item]").dataset.itemId)
-    addToCart(itemId)
+    const id = e.target.closest("[data-store-item]").dataset.itemId
+    addToCart(parseInt(id))
   })
 
   items.forEach(renderStoreItem)
@@ -21,20 +21,20 @@ export function setupStore() {
 function renderStoreItem(item) {
   const storeItem = storeItemTemplate.content.cloneNode(true)
 
-  const container = storeItem.querySelector("[data-item]")
+  const container = storeItem.querySelector("[data-store-item]")
   container.dataset.itemId = item.id
-
-  const image = storeItem.querySelector("[data-image]")
-  image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
-
-  const category = storeItem.querySelector("[data-category]")
-  category.innerText = item.category
 
   const name = storeItem.querySelector("[data-name]")
   name.innerText = item.name
 
+  const category = storeItem.querySelector("[data-category]")
+  category.innerText = item.category
+
+  const image = storeItem.querySelector("[data-image]")
+  image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
+
   const price = storeItem.querySelector("[data-price]")
   price.innerText = formatCurrency(item.priceCents / 100)
 
-  storeItems.appendChild(storeItem)
+  storeItemContainer.appendChild(storeItem)
 }
